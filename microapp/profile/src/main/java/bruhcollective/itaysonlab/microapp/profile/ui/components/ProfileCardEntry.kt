518f472b779117db3fc9534b07db43ad2ca08b70
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2022-08-29 12:21:36 +0300

    [Profile] implement GameCollector card

--- microapp/profile/src/main/java/bruhcollective/itaysonlab/microapp/profile/ui/components/ProfileCardEntry.kt
+++ microapp/profile/src/main/java/bruhcollective/itaysonlab/microapp/profile/ui/components/ProfileCardEntry.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -66,7 +65,7 @@ internal fun ProfileCardEntry(
 
         when (entry.customizationType) {
             EProfileCustomizationType.k_EProfileCustomizationTypeFavoriteGame -> FavoriteGame(entry, ownedGames, achievementsProgress)
-            EProfileCustomizationType.k_EProfileCustomizationTypeGameCollector -> GameCollector()
+            EProfileCustomizationType.k_EProfileCustomizationTypeGameCollector -> GameCollector(entry, ownedGames)
             else -> Text("Unsupported!")
         }
 
