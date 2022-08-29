7c2ae596c0c7e582858b7ffdea9da5f4b7938d3a
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2022-08-29 13:20:46 +0300

    [GamePage] use @Query for request

--- data/src/main/java/bruhcollective/itaysonlab/jetisteam/service/GameService.kt
+++ data/src/main/java/bruhcollective/itaysonlab/jetisteam/service/GameService.kt
@@ -2,10 +2,11 @@ package bruhcollective.itaysonlab.jetisteam.service
 
 import bruhcollective.itaysonlab.jetisteam.models.GameDetails
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GameService {
-    @GET("/api/libraryappdetails/?appid={appId}")
+    @GET("/api/libraryappdetails/")
     suspend fun getGameDetails(
-        appId: String
+        @Query("appId") appId: String
     ): GameDetails
 }
