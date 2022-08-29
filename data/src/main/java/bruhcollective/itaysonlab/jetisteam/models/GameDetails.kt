51d04c32714e3d6f6cc26abb06188598fbb9d6e6
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-08-29 19:06:21 +0200

    [data] improve gamedetails
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- data/src/main/java/bruhcollective/itaysonlab/jetisteam/models/GameDetails.kt
+++ data/src/main/java/bruhcollective/itaysonlab/jetisteam/models/GameDetails.kt
@@ -4,31 +4,27 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class GameDetails(
+class GameDetails(
+    val status: Int,
     val appid: String,
     val name: String,
-    @Json(name = "rgDevelopers") val developers: List<Developer>,
-    @Json(name = "rgFranchises") val franchises: List<Franchise>,
-    @Json(name = "rgPublishers") val publishers: List<Publisher>,
-    val status: Int,
+    @Json(name = "rgDevelopers") val developers: List<NameUrlRelation>,
+    @Json(name = "rgFranchises") val franchises: List<NameUrlRelation>,
+    @Json(name = "rgPublishers") val publishers: List<NameUrlRelation>,
+    @Json(name = "rgSocialMedia") val socialMedia: Map<String, SocialMediaRelation>,
+    @Json(name = "strSnippet") val snippet: String,
     @Json(name = "strFullDescription") val fullDescription: String,
-    val strSnippet: String
 )
 
 @JsonClass(generateAdapter = true)
-data class Developer(
+class NameUrlRelation(
     val name: String,
-    val url: String
+    val url: String?
 )
 
 @JsonClass(generateAdapter = true)
-data class Franchise(
-    val name: String,
-    val url: String
-)
-
-@JsonClass(generateAdapter = true)
-data class Publisher(
-    val name: String,
-    val url: String
+class SocialMediaRelation(
+    val username: String,
+    val url: String,
+    @Json(name = "is_valid") val isValid: Int
 )
