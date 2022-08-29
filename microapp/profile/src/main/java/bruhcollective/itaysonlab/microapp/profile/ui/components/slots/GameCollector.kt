b2a49e4c9944b0daa0671054ebe972a5dc610fbf
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-08-29 19:06:35 +0200

    [profile] optimize + improve gamecollector card
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- microapp/profile/src/main/java/bruhcollective/itaysonlab/microapp/profile/ui/components/slots/GameCollector.kt
+++ microapp/profile/src/main/java/bruhcollective/itaysonlab/microapp/profile/ui/components/slots/GameCollector.kt
@@ -1,15 +1,15 @@
 package bruhcollective.itaysonlab.microapp.profile.ui.components.slots
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -26,34 +26,33 @@ internal fun GameCollector(
     onGameClick: (Int) -> Unit
 ) {
     val games = remember(entry.slots) {
-        val appIds = entry.slots.map { it.appId }
-        ownedGames.filter { appIds.contains(it.value.appid) }.values.toList()
-    }
-
-    LazyRow {
-        items(games.size) {
-            val game = games[it]
-
-            Column(
-                modifier = Modifier.clickable(
-                    onClick = { onGameClick(game.appid) }
-                )
-            ) {
-                AsyncImage(
-                    model = remember(game.appid) {
-                        CdnUrlUtil.buildAppUrl(game.appid, "capsule_467x181.jpg")
-                    },
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                )
-            }
+        entry.slots.mapNotNull { slot ->
+            ownedGames[slot.appId]
         }
     }
+
+    LazyRow(
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(games) { game ->
+            AsyncImage(
+                model = remember(game.appid) {
+                    CdnUrlUtil.buildAppUrl(game.appid, "capsule_231x87.jpg")
+                },
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier
+                    .clickable { onGameClick(game.appid) }
+                    .wrapContentSize()
+                    .height(70.dp)
+            )
+        }
+    }
+
     Column(
         Modifier
-            .drawBehind {
-                drawRect(Color.Black.copy(alpha = 0.15f))
-            }
+            .background(Color.Black.copy(alpha = 0.15f))
             .padding(horizontal = 16.dp, vertical = 12.dp)
             .fillMaxWidth()
     ) {
