65c693c4a15b182b25c535ee0134db54ccda3e1a
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-08-30 22:57:10 +0200

    [data] bbcode parser: implement lists + bold support
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- data/src/main/java/bruhcollective/itaysonlab/jetisteam/util/SteamBbToMarkdown.kt
+++ data/src/main/java/bruhcollective/itaysonlab/jetisteam/util/SteamBbToMarkdown.kt
@@ -4,8 +4,12 @@ object SteamBbToMarkdown {
     fun bbcodeToMarkdown(src: String): String {
         return src
             .replace("\r", "")
+            .replace("[*]", "* ")
+            .replace("[list]", "")
+            .replace("[/list]", "")
             .replaceTagWith("h2") { "# $it" }
             .replaceTagWith("img") { "![image]($it)" }
+            .replaceTagWith("b") { "**$it**" }
     }
 
     private fun String.replaceTagWith(tag: String, func: (String) -> String) = replace(
