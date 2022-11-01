c5ea1c8f14be65f4e9960ddeaa44e43008e902f6
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-10-31 19:38:57 -0600

    data: extend miniprofile with ingame data
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- data/src/main/java/bruhcollective/itaysonlab/jetisteam/models/Miniprofile.kt
+++ data/src/main/java/bruhcollective/itaysonlab/jetisteam/models/Miniprofile.kt
@@ -9,13 +9,21 @@ class Miniprofile (
     @Json(name = "avatar_url") val avatarUrl: String,
     @Json(name = "persona_name") val personaName: String,
     @Json(name = "favorite_badge") val favoriteBadge: MiniprofileBadge?,
+    @Json(name = "in_game") val inGame: MiniprofileIngame?,
 )
 
 @JsonClass(generateAdapter = true)
-class MiniprofileBadge (
+class MiniprofileBadge(
     val name: String,
     val xp: String,
     val level: Int,
     val description: String,
     val icon: String
+)
+
+@JsonClass(generateAdapter = true)
+class MiniprofileIngame(
+    val name: String,
+    val logo: String,
+    @Json(name = "rich_presence") val richPresence: String
 )
