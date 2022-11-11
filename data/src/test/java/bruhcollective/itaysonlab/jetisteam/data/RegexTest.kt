c69c4c61a2f1b00b3a1d81b31af36f0b7762b53e
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-11-11 14:13:54 -0600

    data: fix unit test
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- data/src/test/java/bruhcollective/itaysonlab/jetisteam/data/RegexTest.kt
+++ data/src/test/java/bruhcollective/itaysonlab/jetisteam/data/RegexTest.kt
@@ -2,13 +2,15 @@ package bruhcollective.itaysonlab.jetisteam.data
 
 import bruhcollective.itaysonlab.jetisteam.util.Regexes
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 
 class RegexTest {
     @Test
     fun steamSignIn_parsesCorrectly() {
         val data = Regexes.extractDataFromQr("https://s.team/q/1/1234567890")
-        assertEquals(data.version, 1)
+        assertNotNull(data)
+        assertEquals(data!!.version, 1)
         assertEquals(data.sessionId, 1234567890L)
     }
 }
