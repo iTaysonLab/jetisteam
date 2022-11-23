c8c6626292eec4c3e6df7a6dae136a496b18de0a
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-11-22 19:42:27 -0600

    app: enable predictive back + file provider
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

---
+++ app/src/main/java/bruhcollective/itaysonlab/jetisteam/JsFileProvider.kt
@@ -0,0 +1,5 @@
+package bruhcollective.itaysonlab.jetisteam
+
+import androidx.core.content.FileProvider
+
+class JsFileProvider: FileProvider(R.xml.provider_paths)
--- app/src/main/java/bruhcollective/itaysonlab/jetisteam/MainActivity.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetisteam/MainActivity.kt
@@ -3,19 +3,26 @@ package bruhcollective.itaysonlab.jetisteam
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.view.WindowCompat
 import bruhcollective.itaysonlab.jetisteam.ui.screens.AppNavigation
 import bruhcollective.itaysonlab.jetisteam.ui.theme.JetisteamTheme
+import bruhcollective.itaysonlab.microapp.core.navigation.extensions.delegates.LocalShareDispatcher
+import bruhcollective.itaysonlab.microapp.core.navigation.extensions.delegates.ShareDispatcher
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val shareDispatcher = ShareDispatcher(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            JetisteamTheme {
-                AppNavigation()
+            CompositionLocalProvider(LocalShareDispatcher provides shareDispatcher) {
+                JetisteamTheme {
+                    AppNavigation()
+                }
             }
         }
     }
